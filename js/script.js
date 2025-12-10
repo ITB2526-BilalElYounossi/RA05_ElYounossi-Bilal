b751764833d213782acca1be187233e101e77e94
Bilal El Younossi <bilal.elyounossi.7e9@itb.cat>
2025-12-10 12:37:26 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('data').textContent = new Date().getFullYear();
-document.getElementById('formContacte').addEventListener('submit', (e) => {
-  alert('Gràcies! El teu missatge s’ha enviat.');
-  e.preventDefault();
-});
-// Añade botón en HTML y cambia clase en <body>
